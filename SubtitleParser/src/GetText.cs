e8d1f7fb34ed1df85364e522a6087cedb21390a0
Muhammed Enes Okullu <meokullu@gmail.com>
2024-03-17 12:02:20 +0300

    v2.2.2

---
+++ SubtitleParser/src/GetText.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleParser
+{
+    public partial class SubtitleParser
+    {
+        /// <summary>
+        /// Returns lines of contents of specified file via its path.
+        /// </summary>
+        /// <param name="filePath">File path that content will be read.</param>
+        /// <returns>Lines of contents (string[])</returns>
+        public static string[] GetTextByLines(string filePath)
+        {
+            // String array variable that will be filled with content.
+            string[] movieSubtitleTextByLines;
+
+            try
+            {
+                // Getting lines of file.
+                movieSubtitleTextByLines = File.ReadAllLines(path: filePath);
+            }
+            catch (Exception ex)
+            {
+                // Returning exception.
+                throw ex;
+            }
+
+            // Returning lines
+            return movieSubtitleTextByLines;
+        }
+
+        /// <summary>
+        /// [Unsafe] Returns lines of contents of specified file via its path.
+        /// </summary>
+        /// <param name="filePath">File path that content will be read.</param>
+        /// <returns>Lines of contents (string[])</returns>
+        public static string[] GetTextByLinesUnsafe(string filePath)
+        {
+            // Getting lines of file.
+            string[] movieSubtitleTextByLines = File.ReadAllLines(path: filePath);
+
+            // Returning lines
+            return movieSubtitleTextByLines;
+        }
+    }
+}
---
+++ SubtitleParser/src/Trim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleParser
+{
+    public partial class SubtitleParser
+    {
+        // Variable list hold line number of trimmed lines.
+        private static readonly List<int> s_trimmedLineIndexList = new List<int>();
+
+        /// <summary>
+        /// List variable that holds order numbers that are trimmed by ParseSubtitleList() or ParseSubitleListUnsafe()
+        /// </summary>
+        public static readonly List<int> TrimmedLineIndexList = s_trimmedLineIndexList;
+    }
+}
